165af93ed9b95b550c3bb7d981164333fa027bb7
Vedant Bhatt <51319511+vvicky30@users.noreply.github.com>
2019-12-06 04:40:59 +0530

    illustration of recycling process of vectors in R
    here we can analyse that how the one vecor is used to recycle its elements whenever its arithmetically operated with another vector with larger number of elements

---
+++ recyclevectors.R
@@ -0,0 +1,15 @@
+#vecrorized operation
+x<-c(1,4,5,6,7)
+y<-c(4,8,7,9,6)
+a<-x+y
+rm(z)
+s<-x-y
+p<-x*y
+d<-x/y
+#recycling of vectors
+f<-c(1,2,3,4,5)
+g<-c(1,2,3,4,5,6,7,8,9,10)
+h<-f+g
+v<-c(1,2,3,4,5,6)
+b<-c(1,2,3,4,5,6,7,8,10)
+n<-v+b
